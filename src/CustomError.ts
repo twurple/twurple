dd003b8629079f616d70365ab62387b8db81702e
Daniel Fischer <fischer.daniel94@googlemail.com>
2017-08-01 23:56:26 +0200

    remove useless comment

--- src/CustomError.ts
+++ src/CustomError.ts
@@ -1,6 +1,6 @@
 export default class CustomError extends Error {
 	constructor(message: string) {
-		super(); // (A)
+		super();
 		this.name = this.constructor.name;
 		this.message = message;
 		Error.captureStackTrace(this, this.constructor);
