f4572a47df029185b2185305a093c9803bee1d92
Daniel Fischer <fischer.daniel94@gmail.com>
2018-08-29 22:18:02 +0200

    travis build docs (2)

--- travis/publishDocs.sh
+++ travis/publishDocs.sh
@@ -7,10 +7,11 @@ npm run docs
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git clone git://github.com/d-fischer/d-fischer.github.io
+git clone git://${GH_TOKEN}@github.com/d-fischer/d-fischer.github.io
 rm -rfv d-fischer.github.io/twitch
 mv -fv generatedDocs d-fischer.github.io/twitch
 
 cd d-fischer.github.io
-git commit -A -m "Travis build of twitch: $TRAVIS_BUILD_NUMBER"
+git add .
+git commit -m "Travis build of twitch: $TRAVIS_BUILD_NUMBER"
 git push --quiet --set-upstream origin master
