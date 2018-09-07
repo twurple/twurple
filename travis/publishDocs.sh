1948716df4c1e9aca04def06abbe63ede164ec48
Daniel Fischer <fischer.daniel94@gmail.com>
2018-09-08 00:40:05 +0200

    new doc publish script, should work regardless of repo

--- travis/publishDocs.sh
+++ travis/publishDocs.sh
@@ -4,16 +4,19 @@ set -e
 
 npm run docs
 
+REPO_USER="${TRAVIS_REPO_SLUG%/*}"
+REPO_NAME="${TRAVIS_REPO_SLUG#*/}"
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git clone https://${GH_TOKEN}@github.com/d-fischer/d-fischer.github.io.git
-rm -rfv d-fischer.github.io/twitch
-mv -fv generatedDocs d-fischer.github.io/twitch
+git clone https://${GH_TOKEN}@github.com/${REPO_USER}/${REPO_USER}.github.io.git docRepo
+rm -rfv docRepo/${REPO_NAME}
+mv -fv generatedDocs docRepo/${REPO_NAME}
 
-cd d-fischer.github.io
+cd docRepo
 if [[ `git status --porcelain` ]]; then
 	git add .
-	git commit -m "Travis build of twitch: $TRAVIS_BUILD_NUMBER"
+	git commit -m "Travis build of ${REPO_NAME}: ${TRAVIS_BUILD_NUMBER}"
 	git push --quiet --set-upstream origin master
 fi
