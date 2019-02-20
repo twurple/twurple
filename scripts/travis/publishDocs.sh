e95968e3db9eeddd716ed8e78bb30efc33d761d1
Daniel Fischer <fischer.daniel94@gmail.com>
2019-02-21 00:58:56 +0100

    publish docs for branches

--- scripts/travis/publishDocs.sh
+++ scripts/travis/publishDocs.sh
@@ -7,16 +7,22 @@ npm run docs
 REPO_USER="${TRAVIS_REPO_SLUG%/*}"
 REPO_NAME="${TRAVIS_REPO_SLUG#*/}"
 
+if [[ $TRAVIS_BRANCH = "master" ]]; then
+	REPO_FULL_NAME="${REPO_NAME}"
+else
+	REPO_FULL_NAME="${REPO_NAME}@${TRAVIS_BRANCH}"
+fi
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 git clone https://${GH_TOKEN}@github.com/${REPO_USER}/${REPO_USER}.github.io.git docRepo
-rm -rfv docRepo/${REPO_NAME}
-mv -fv generatedDocs docRepo/${REPO_NAME}
+rm -rfv docRepo/${REPO_FULL_NAME}
+mv -fv generatedDocs docRepo/${REPO_FULL_NAME}
 
 cd docRepo
 if [[ `git status --porcelain` ]]; then
 	git add .
-	git commit -m "Travis build of ${REPO_NAME}: ${TRAVIS_BUILD_NUMBER}"
+	git commit -m "Travis build of ${REPO_FULL_NAME}: ${TRAVIS_BUILD_NUMBER}"
 	git push --quiet --set-upstream origin master
 fi
