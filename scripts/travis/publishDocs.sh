1906aa5664d10531aef5bb9d51484a5b8c9ad3d6
Daniel Fischer <fischer.daniel94@gmail.com>
2019-03-10 15:40:04 +0100

    fix doc build setting the author globally

--- scripts/travis/publishDocs.sh
+++ scripts/travis/publishDocs.sh
@@ -7,14 +7,15 @@ npm run docs
 REPO_USER="${TRAVIS_REPO_SLUG%/*}"
 REPO_NAME="${TRAVIS_REPO_SLUG#*/}"
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
 git clone https://${GH_TOKEN}@github.com/${REPO_USER}/${REPO_USER}.github.io.git docRepo
 rm -rfv docRepo/${REPO_NAME}
 mv -fv generatedDocs docRepo/${REPO_NAME}
 
 cd docRepo
+
+git config user.email "travis@travis-ci.org"
+git config user.name "Travis CI"
+
 if [[ `git status --porcelain` ]]; then
 	git add .
 	git commit -m "Travis build of ${REPO_NAME}: ${TRAVIS_BUILD_NUMBER}"
