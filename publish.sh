4895199bff9cf79e8d8e2dd7c4bbbdca10a23f76
Daniel Fischer <fischer.daniel94@gmail.com>
2019-03-13 23:26:40 +0100

    remove version placeholder (independent versioning makes this impossible)

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ yarn lint
 yarn build
 
 VERSIONTYPE="${1:-patch}"
-lerna version --no-push --preid pre ${VERSIONTYPE} -m "release version %s"
+lerna version --no-push --preid pre ${VERSIONTYPE} -m "release new versions"
 case ${VERSIONTYPE} in
 	"pre"*) lerna publish from-package --dist-tag next ;;
 	*) lerna publish from-package ;;
