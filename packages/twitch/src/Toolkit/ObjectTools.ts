9fda1bbc25a11b1fbd307c59762e3d2f53c01bdb
Daniel Fischer <fischer.daniel94@gmail.com>
2019-06-15 23:29:16 +0200

    fix build

--- packages/twitch/src/Toolkit/ObjectTools.ts
+++ packages/twitch/src/Toolkit/ObjectTools.ts
@@ -36,7 +36,7 @@ export function indexBy<T>(arr: T[], keyFn: KeyMapper<T>): Record<string, T>;
 export function indexBy<T>(arr: T[], keyFn: Extract<keyof T, string> | KeyMapper<T>): Record<string, T> {
 	if (typeof keyFn !== 'function') {
 		const key = keyFn;
-		keyFn = ((value: T) => value[key].toString()) as KeyMapper<T>;
+		keyFn = ((value: T) => (value[key] as unknown as object).toString()) as KeyMapper<T>;
 	}
 	return objectFromArray<T, T, Record<string, T>>(arr, val => ({ [(keyFn as KeyMapper<T>)(val)]: val }));
 }
