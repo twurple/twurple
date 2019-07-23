b6bb1e0537e3828172a22b6b65b99be190f84021
Daniel Fischer <daniel@d-fischer.dev>
2019-07-23 23:18:32 +0200

    make publish script work without lerna in PATH

--- publish.sh
+++ publish.sh
@@ -8,9 +8,9 @@ yarn lint
 yarn build
 
 VERSIONTYPE="${1:-patch}"
-lerna version --no-push --no-commit-hooks --preid pre ${VERSIONTYPE} -m "release new versions"
+yarn lerna version --no-push --no-commit-hooks --preid pre ${VERSIONTYPE} -m "release new versions"
 case ${VERSIONTYPE} in
-	"pre"*) lerna publish from-package --dist-tag next ;;
-	*) lerna publish from-package ;;
+	"pre"*) yarn lerna publish from-package --dist-tag next ;;
+	*) yarn lerna publish from-package ;;
 esac
 cd "$CWD"
