44b9f41db6852ebf0185f0199ff6088dbb9265fa
Daniel Fischer <daniel@d-fischer.dev>
2019-08-18 15:09:28 +0200

    fix custom error prototype chain

--- packages/twitch/src/Errors/CustomError.ts
+++ packages/twitch/src/Errors/CustomError.ts
@@ -3,8 +3,19 @@ export default class CustomError extends Error {
 	constructor(...params: [string, string?, string?]) {
 		// @ts-ignore
 		super(...params);
+
+		// restore prototype chain
+		const actualProto = new.target.prototype;
+
+		if (Object.setPrototypeOf) {
+			Object.setPrototypeOf(this, actualProto);
+		} else {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			(this as any).__proto__ = actualProto;
+		}
+
 		if (Error.captureStackTrace) {
-			Error.captureStackTrace(this, this.constructor);
+			Error.captureStackTrace(this, new.target.constructor);
 		}
 	}
 
