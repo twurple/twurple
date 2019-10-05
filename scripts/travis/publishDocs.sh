559fb8f403a93aef8df8a1c4621c8ea65e8920a6
Daniel Fischer <daniel@d-fischer.dev>
2019-10-05 20:34:32 +0200

    update documen.ts

--- scripts/travis/publishDocs.sh
+++ scripts/travis/publishDocs.sh
@@ -6,7 +6,7 @@ REPO_USER="${TRAVIS_REPO_SLUG%/*}"
 
 npm run docs -- --base-url "/"
 
-git clone https://${GH_TOKEN}@github.com/${REPO_USER}/${REPO_USER}.github.io.git docRepo
+git clone "https://${GH_TOKEN}@github.com/${REPO_USER}/${REPO_USER}.github.io.git" docRepo
 rm -rfv docRepo/*
 mv -fv generatedDocs/* docRepo/
 
@@ -15,7 +15,7 @@ cd docRepo
 git config user.email "travis@travis-ci.org"
 git config user.name "Travis CI"
 
-if [[ `git status --porcelain` ]]; then
+if [[ $(git status --porcelain) ]]; then
 	git add .
 	git commit -m "Travis build: ${TRAVIS_BUILD_NUMBER}"
 	git push --quiet --set-upstream origin master
