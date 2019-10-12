a44ef75e4ad2aedc3077c9263bc69fda23d67a03
Daniel Fischer <daniel@d-fischer.dev>
2019-10-12 14:03:41 +0200

    add version number to automatic release commit

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ yarn prettier:check
 yarn build
 
 VERSIONTYPE="${1:-patch}"
-yarn lerna version --no-push --no-commit-hooks --preid pre ${VERSIONTYPE} -m "release new versions"
+yarn lerna version --no-push --no-commit-hooks --preid pre ${VERSIONTYPE} -m "release version %v"
 case ${VERSIONTYPE} in
 	"pre"*) yarn lerna publish from-package --dist-tag next ;;
 	*) yarn lerna publish from-package ;;
