2ef458724baf6c9c205630998b64ba774c181576
Daniel Fischer <daniel@d-fischer.dev>
2020-05-16 18:56:28 +0200

    make emote parsing more safe

--- packages/twitch-chat-client/src/Toolkit/EmoteTools.ts
+++ packages/twitch-chat-client/src/Toolkit/EmoteTools.ts
@@ -34,10 +34,16 @@ export function parseEmoteOffsets(emotes?: string): Map<string, string[]> {
 	}
 
 	return new Map(
-		emotes.split('/').map(emote => {
-			const [emoteId, placements] = emote.split(':', 2);
-			return [emoteId, placements.split(',')] as [string, string[]];
-		})
+		emotes
+			.split('/')
+			.map(emote => {
+				const [emoteId, placements] = emote.split(':', 2);
+				if (!placements) {
+					return null;
+				}
+				return [emoteId, placements.split(',')] as [string, string[]];
+			})
+			.filter((e): e is [string, string[]] => e !== null)
 	);
 }
 
