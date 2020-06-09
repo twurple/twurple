4d717f3a059897cac1ffc206e907522086802e79
Daniel Fischer <daniel@d-fischer.dev>
2020-06-09 21:11:54 +0200

    update naming convention with all twitch properties

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,106 @@
 const path = require('path');
+const baseRules = require('@d-fischer/eslint-config');
+
+const memberNames = [
+	// Twitch API
+	'_(id|name|at)$',
+	'^user_login$',
+	'^client_secret$',
+	'^redirect_uri$',
+	'^(access|refresh|auth)_token$',
+	'^has_delay$',
+	'^include_sponsored$',
+	'^stream_(key|type)$',
+	'^broadcaster_(language|type)$',
+	'^grant_type$',
+	'^expires_in$',
+	'^badge_sets$',
+	'^profile_banner(_background_color)?$',
+	'^video_banner$',
+	'^channel_feed_enabled$',
+	'^(start|end)_time$',
+	'^((cover|offline|profile)_image|edit|embed|thumbnail|box_art|click)_url$',
+	'^click_action$',
+	'^(image_)?url_\\dx$',
+	'^emoticon_set$',
+	'^is_(anonymous|gift|user_input_required|sub_only|enabled|paused|in_stock|previewable|playlist|(verified|known)_bot)$',
+	'^minimum_allowed_role$',
+	'^(chatter|view(er)?)_count$',
+	'^min_bits$',
+	'^date_range$',
+	'^event_(type|timestamp|data)$',
+	'^product_(type|data)$',
+	'^community_ids$',
+	'^position_seconds$',
+	'^(default|profile)_image$',
+	'^background_color$',
+	'^max_per_stream$',
+	'^should_redemptions_skip_request_queue$',
+	'^user_input$',
+	'^hub.',
+	'^email_verified$',
+	'^twitter_connected$',
+	'^chat_message$',
+	'^badge_tier$',
+	'^message_type$',
+	'^(previous|new)_version$',
+	'^(total_)?bits_used$',
+	'^badge_entitlement$',
+	'^moderation_action$',
+	'^created_by$',
+	'^sub_(plan|message)$',
+	'^user_type$',
+	'^sent_ts$',
+	'^data_object$',
+	'^video_height$',
+	'^average_fps$',
+	// HTTP
+	'^Accept$'
+];
+
+const enumNames = [
+	// Twitch API
+	'x1_5'
+];
+
+const namingConvention = [...baseRules.rules['@typescript-eslint/naming-convention']].map(rule => {
+	if (typeof rule === 'object') {
+		if (rule.selector === 'default' && !rule.modifiers) {
+			return {
+				...rule,
+				filter: {
+					match: false,
+					regex: [...memberNames, ...enumNames].join('|')
+				}
+			};
+		}
+		if (rule.selector === 'memberLike' && !rule.modifiers) {
+			return {
+				...rule,
+				filter: {
+					match: false,
+					regex: [...memberNames, ...enumNames].join('|')
+				}
+			};
+		}
+		if (rule.selector === 'enumMember' && !rule.modifiers) {
+			return {
+				...rule,
+				filter: {
+					match: false,
+					regex: enumNames.join('|')
+				}
+			};
+		}
+	}
+
+	return rule;
+});
 
 module.exports = {
 	extends: ['@d-fischer'],
 	rules: {
-		'@typescript-eslint/camelcase': [
-			'error',
-			{
-				// only allow stuff Twitch uses
-				allow: [
-					'_(id|name)$',
-					'user_login',
-					'client_secret',
-					'redirect_uri',
-					'(created|updated|started|ended)_at',
-					'(access|refresh|auth)_token',
-					'has_delay',
-					'include_sponsored',
-					'x1_5',
-					'stream_(key|type)',
-					'broadcaster_language',
-					'grant_type'
-				]
-			}
-		]
+		'@typescript-eslint/naming-convention': namingConvention
 	},
 	settings: {
 		'import/resolver': {
