ea88e30e68fcd870a20d2ccc1fcd7c9ba555de0b
Daniel Fischer <daniel@d-fischer.dev>
2020-12-06 12:23:50 +0100

    use status parameter for redemptions list
    fixes #193

--- packages/twitch/src/API/Helix/ChannelPoints/HelixChannelPointsApi.ts
+++ packages/twitch/src/API/Helix/ChannelPoints/HelixChannelPointsApi.ts
@@ -298,6 +298,7 @@ export class HelixChannelPointsApi extends BaseApi {
 			query: {
 				broadcaster_id: extractUserId(broadcaster),
 				reward_id: rewardId,
+				status,
 				sort: filter.newestFirst ? 'NEWEST' : 'OLDEST',
 				...makePaginationQuery(filter)
 			}
@@ -329,6 +330,7 @@ export class HelixChannelPointsApi extends BaseApi {
 				query: {
 					broadcaster_id: extractUserId(broadcaster),
 					reward_id: rewardId,
+					status,
 					sort: filter.newestFirst ? 'NEWEST' : 'OLDEST'
 				}
 			},
