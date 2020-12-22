46c2febcf3f641c472246366fe71610dfc41b453
Sam Hirst <samuel.hirst@student.manchester.ac.uk>
2020-12-22 10:57:29 +0000

    moved valid names regex outside of function and specified bad names in error message

--- packages/twitch-chat-client/src/Toolkit/UserTools.ts
+++ packages/twitch-chat-client/src/Toolkit/UserTools.ts
@@ -1,12 +1,13 @@
+// valid names must be alphanumberic or underscores between 4 and 25 characters (cannot start with underscore)
+const validNames = /^[a-zA-Z0-9][a-zA-Z0-9_]{3,24}$/;
+
 /** @private */
 export function toUserName(channel: string): string {
 	// it's okay if this is already a user name, we only remove the first character if it's a pound
 	const name = channel.replace(/^#/, '').toLowerCase();
-	// valid usernames must be alphanumberic or underscores between 4 and 25 characters (cannot start with underscore)
-	const re = RegExp(/^[a-zA-Z0-9][a-zA-Z0-9_]{3,24}$/);
-	if (!re.test(name)) {
+	if (!validNames.test(name)) {
 		throw new Error(
-			'Invalid channel or username. It must be made from alphanumberic characters and between 4 and 25 characters in length. Usernames and channels names cannot start with an underscore.'
+			'"$[name}" is not a valid user or channel name. It must be 4-25 characters long, can only include letters, numbers and underscores, and can not start with an underscore.'
 		);
 	}
 	return name;
