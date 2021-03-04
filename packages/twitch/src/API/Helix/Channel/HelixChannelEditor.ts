6cab65d430f504d7aac7057739d6f4f6fc977a69
Daniel Fischer <daniel@d-fischer.dev>
2021-03-05 00:08:56 +0100

    add Get Channel Edtiors

---
+++ packages/twitch/src/API/Helix/Channel/HelixChannelEditor.ts
@@ -0,0 +1,52 @@
+import { Enumerable } from '@d-fischer/shared-utils';
+import type { ApiClient } from '../../../ApiClient';
+import type { HelixUser } from '../User/HelixUser';
+
+/** @private */
+export interface HelixChannelEditorData {
+	user_id: string;
+	user_name: string;
+	created_at: string;
+}
+
+/**
+ * An editor of a previously given channel.
+ */
+export class HelixChannelEditor {
+	@Enumerable(false) private readonly _data: HelixChannelEditorData;
+	@Enumerable(false) private readonly _client: ApiClient;
+
+	/** @private */
+	constructor(data: HelixChannelEditorData, client: ApiClient) {
+		this._data = data;
+		this._client = client;
+	}
+
+	/**
+	 * The ID of the user.
+	 */
+	get id(): string {
+		return this._data.user_id;
+	}
+
+	/**
+	 * The display name of the user.
+	 */
+	get displayName(): string {
+		return this._data.user_name;
+	}
+
+	/**
+	 * Retrieves additional information about the user.
+	 */
+	async getUser(): Promise<HelixUser | null> {
+		return this._client.helix.users.getUserById(this._data.user_id);
+	}
+
+	/**
+	 * The date when the user was given editor status.
+	 */
+	get creationDate(): Date {
+		return new Date(this._data.created_at);
+	}
+}
