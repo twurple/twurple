22550a92dea492521d974389195c55b6dc5d0a48
Daniel Fischer <daniel@d-fischer.dev>
2022-04-24 23:14:00 +0200

    remove error proto workaround for very old clients (that are already unsupported)

--- packages/common/src/errors/CustomError.ts
+++ packages/common/src/errors/CustomError.ts
@@ -10,15 +10,7 @@ export class CustomError extends Error {
 		super(message, options);
 
 		// restore prototype chain
-		const actualProto = new.target.prototype;
-
-		// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-		if (Object.setPrototypeOf) {
-			Object.setPrototypeOf(this, actualProto);
-		} else {
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access
-			(this as any).__proto__ = actualProto;
-		}
+		Object.setPrototypeOf(this, new.target.prototype);
 
 		// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 		Error.captureStackTrace?.(this, new.target.constructor);
