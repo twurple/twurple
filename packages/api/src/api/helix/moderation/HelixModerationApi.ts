ebce91d94d1b029c2954d95686263ac3bb634fd2
Zack Williamson <zwilliamson@outlook.com>
2022-07-31 09:44:18 -0400

    Add/Remove Channel Moderators via Helix (#374)
    * Add/Remove Channel Moderators via Helix
    
    * Update docs
    
    * Update HelixModerationApi.ts
    
    Co-authored-by: Daniel Fischer <daniel@d-fischer.dev>

--- packages/api/src/api/helix/moderation/HelixModerationApi.ts
+++ packages/api/src/api/helix/moderation/HelixModerationApi.ts
@@ -489,4 +489,42 @@ export class HelixModerationApi extends BaseApi {
 			}
 		});
 	}
+
+	/**
+	 * Adds a moderator to the broadcaster’s chat room.
+	 *
+	 * @param broadcaster The ID of the broadcaster that owns the chat room. This ID must match the user ID in the access token.
+	 * @param user The ID of the user to add as a moderator in the broadcaster’s chat room.
+	 */
+	async addChannelModerator(broadcaster: UserIdResolvable, user: UserIdResolvable): Promise<void> {
+		await this._client.callApi({
+			type: 'helix',
+			url: 'moderation/moderators',
+			method: 'POST',
+			scope: 'channel:manage:moderators',
+			query: {
+				broadcaster_id: extractUserId(broadcaster),
+				user_id: extractUserId(user)
+			}
+		});
+	}
+
+	/**
+	 * Removes a moderator from the broadcaster’s chat room.
+	 *
+	 * @param broadcaster The ID of the broadcaster that owns the chat room. This ID must match the user ID in the access token.
+	 * @param user The ID of the user to remove as a moderator from the broadcaster’s chat room.
+	 */
+	async removeChannelModerator(broadcaster: UserIdResolvable, user: UserIdResolvable): Promise<void> {
+		await this._client.callApi({
+			type: 'helix',
+			url: 'moderation/moderators',
+			method: 'DELETE',
+			scope: 'channel:manage:moderators',
+			query: {
+				broadcaster_id: extractUserId(broadcaster),
+				user_id: extractUserId(user)
+			}
+		});
+	}
 }
