c4fe6c3d25f96c323464de0b31e8f991e793199c
Stimul Cross <stimulcross@gmail.com>
2022-12-14 05:23:09 +0600

    feat(api): use string literal type for broadcasterType in HelixUser (#421)
    * feat(api): use string literal type for broadcasterType in HelixUser
    
    * fix(eventsub): import missing HelixBroadcasterType
    
    Co-authored-by: Daniel Fischer <daniel@d-fischer.dev>

--- packages/api/src/api/helix/user/HelixUser.ts
+++ packages/api/src/api/helix/user/HelixUser.ts
@@ -2,7 +2,7 @@ import { Enumerable } from '@d-fischer/shared-utils';
 import type { HelixUserType, UserIdResolvable, UserIdResolvableType, UserNameResolveableType } from '@twurple/common';
 import { DataObject, rawDataSymbol, rtfm } from '@twurple/common';
 import type { ApiClient } from '../../../ApiClient';
-import { type HelixUserData } from '../../../interfaces/helix/user.external';
+import { type HelixBroadcasterType, type HelixUserData } from '../../../interfaces/helix/user.external';
 import type { HelixPaginatedResultWithTotal } from '../HelixPaginatedResult';
 import type { HelixStream } from '../stream/HelixStream';
 import type { HelixSubscription } from '../subscriptions/HelixSubscription';
@@ -62,9 +62,9 @@ export class HelixUser extends DataObject<HelixUserData> implements UserIdResolv
 	}
 
 	/**
-	 * The type of the user.
+	 * The type of the broadcaster.
 	 */
-	get broadcasterType(): string {
+	get broadcasterType(): HelixBroadcasterType {
 		return this[rawDataSymbol].broadcaster_type;
 	}
 
