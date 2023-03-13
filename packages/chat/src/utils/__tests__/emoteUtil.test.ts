8f30e4eb48c36ff383319bdc508002a4be844bce
Daniel Fischer <daniel@d-fischer.dev>
2023-03-14 00:28:50 +0100

    add tests for new decoupled emote/cheer parsers

---
+++ packages/chat/src/utils/__tests__/emoteUtil.test.ts
@@ -0,0 +1,21 @@
+import { parseEmoteOffsets } from '../emoteUtil';
+
+describe('Emote offset parser', () => {
+	it('parses a single emote at a single place', () => {
+		expect(parseEmoteOffsets('25:0-4')).toEqual(new Map([['25', ['0-4']]]));
+	});
+
+	it('parses a single emote multiple times', () => {
+		expect(parseEmoteOffsets('25:0-4,6-10,12-16')).toEqual(new Map([['25', ['0-4', '6-10', '12-16']]]));
+	});
+
+	it('parses multiple emotes', () => {
+		expect(parseEmoteOffsets('25:0-4/1902:6-10/305954156:12-19')).toEqual(
+			new Map([
+				['25', ['0-4']],
+				['1902', ['6-10']],
+				['305954156', ['12-19']]
+			])
+		);
+	});
+});
---
+++ packages/common/src/emotes/__tests__/messagePartParser.test.ts
@@ -0,0 +1,152 @@
+import { ChatEmote } from '../ChatEmote';
+import { parseChatMessage } from '../messagePartParser';
+
+describe('Message part parser', () => {
+	it('parses a message without emotes or cheers', () => {
+		const message = 'very interesting message';
+
+		expect(parseChatMessage(message, new Map())).toEqual([
+			{
+				type: 'text',
+				text: message,
+				position: 0,
+				length: 24
+			}
+		]);
+	});
+
+	it('parses a message with emotes', () => {
+		const message = 'very interesting message Kappa truly great';
+		const emoteOffsets = new Map([['25', ['25-29']]]);
+
+		expect(parseChatMessage(message, emoteOffsets)).toEqual([
+			{
+				type: 'text',
+				text: 'very interesting message ',
+				position: 0,
+				length: 25
+			},
+			{
+				type: 'emote',
+				id: '25',
+				name: 'Kappa',
+				position: 25,
+				length: 5,
+				displayInfo: new ChatEmote({
+					id: '25',
+					code: 'Kappa'
+				})
+			},
+			{
+				type: 'text',
+				text: ' truly great',
+				position: 30,
+				length: 12
+			}
+		]);
+	});
+
+	it('parses a message with emotes and cheermotes', () => {
+		const message = 'Happy birthday bday100 PogChamp cheer1000';
+		const emoteOffsets = new Map([['305954156', ['23-30']]]);
+		const cheermoteNames = ['bday', 'cheer'];
+
+		expect(parseChatMessage(message, emoteOffsets, cheermoteNames)).toEqual([
+			{
+				type: 'text',
+				text: 'Happy birthday ',
+				position: 0,
+				length: 15
+			},
+			{
+				type: 'cheer',
+				name: 'bday',
+				amount: 100,
+				position: 15,
+				length: 7
+			},
+			{
+				type: 'text',
+				text: ' ',
+				position: 22,
+				length: 1
+			},
+			{
+				type: 'emote',
+				id: '305954156',
+				name: 'PogChamp',
+				position: 23,
+				length: 8,
+				displayInfo: new ChatEmote({
+					id: '305954156',
+					code: 'PogChamp'
+				})
+			},
+			{
+				type: 'text',
+				text: ' ',
+				position: 31,
+				length: 1
+			},
+			{
+				type: 'cheer',
+				name: 'cheer',
+				amount: 1000,
+				position: 32,
+				length: 9
+			}
+		]);
+	});
+
+	it('parses positions correctly after emoji', () => {
+		const message = 'Happy birthday 🎉 🍰 bday100 PogChamp cheer1000';
+		const emoteOffsets = new Map([['305954156', ['27-34']]]);
+		const cheermoteNames = ['bday', 'cheer'];
+
+		expect(parseChatMessage(message, emoteOffsets, cheermoteNames)).toEqual([
+			{
+				type: 'text',
+				text: 'Happy birthday 🎉 🍰 ',
+				position: 0,
+				length: 19
+			},
+			{
+				type: 'cheer',
+				name: 'bday',
+				amount: 100,
+				position: 19,
+				length: 7
+			},
+			{
+				type: 'text',
+				text: ' ',
+				position: 26,
+				length: 1
+			},
+			{
+				type: 'emote',
+				id: '305954156',
+				name: 'PogChamp',
+				position: 27,
+				length: 8,
+				displayInfo: new ChatEmote({
+					id: '305954156',
+					code: 'PogChamp'
+				})
+			},
+			{
+				type: 'text',
+				text: ' ',
+				position: 35,
+				length: 1
+			},
+			{
+				type: 'cheer',
+				name: 'cheer',
+				amount: 1000,
+				position: 36,
+				length: 9
+			}
+		]);
+	});
+});
