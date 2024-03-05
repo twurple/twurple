8ff72f2498d2ea09be8ade65dd0bcb38daa5eabb
theca11 <48060851+theca11@users.noreply.github.com>
2024-03-05 19:48:41 +0100

    add missing HelixEventSubSubscriptionStatus values
    Based on description at https://dev.twitch.tv/docs/api/reference/#get-eventsub-subscriptions

--- packages/api/src/interfaces/endpoints/eventSub.external.ts
+++ packages/api/src/interfaces/endpoints/eventSub.external.ts
@@ -6,11 +6,19 @@ export type HelixEventSubSubscriptionStatus =
 	| 'enabled'
 	| 'webhook_callback_verification_pending'
 	| 'webhook_callback_verification_failed'
-	| 'websocket_disconnected'
 	| 'notification_failures_exceeded'
 	| 'authorization_revoked'
+	| 'moderator_removed'
 	| 'user_removed'
-	| 'version_removed';
+	| 'version_removed'
+	| 'beta_maintenance'
+	| 'websocket_disconnected'
+	| 'websocket_failed_ping_pong'
+	| 'websocket_received_inbound_traffic'
+	| 'websocket_connection_unused'
+	| 'websocket_internal_error'
+	| 'websocket_network_timeout'
+	| 'websocket_network_error';
 
 /** @private */
 export interface HelixEventSubWebHookTransportData {
