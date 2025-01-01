c70a5e47201a3567f711e9348fc762cdbd4e550f
Stimul Cross <stimulcross@gmail.com>
2025-01-01 18:12:30 +0500

    feat(api): add shared chat session endpoint

---
+++ packages/api/src/endpoints/chat/HelixSharedChatSession.ts
@@ -0,0 +1,62 @@
+import { Enumerable } from '@d-fischer/shared-utils';
+import { checkRelationAssertion, DataObject, rawDataSymbol, rtfm } from '@twurple/common';
+import { type HelixSharedChatSessionData } from '../../interfaces/endpoints/shared-chat-session.external';
+import { type BaseApiClient } from '../../client/BaseApiClient';
+import { type HelixUser } from '../user/HelixUser';
+import { HelixSharedChatSessionParticipant } from './HelixSharedChatSessionParticipant';
+
+/**
+ * A shared chat session.
+ */
+@rtfm<HelixSharedChatSession>('api', 'HelixSharedChatSession', 'sessionId')
+export class HelixSharedChatSession extends DataObject<HelixSharedChatSessionData> {
+	/** @internal */ @Enumerable(false) private readonly _client: BaseApiClient;
+
+	/** @internal */
+	constructor(data: HelixSharedChatSessionData, client: BaseApiClient) {
+		super(data);
+		this._client = client;
+	}
+
+	/**
+	 * The unique identifier for the shared chat session.
+	 */
+	get sessionId(): string {
+		return this[rawDataSymbol].session_id;
+	}
+
+	/**
+	 * The ID of the host broadcaster.
+	 */
+	get hostBroadcasterId(): string {
+		return this[rawDataSymbol].host_broadcaster_id;
+	}
+
+	/**
+	 * Gets information about the host broadcaster.
+	 */
+	async getHostBroadcaster(): Promise<HelixUser> {
+		return checkRelationAssertion(await this._client.users.getUserById(this[rawDataSymbol].host_broadcaster_id));
+	}
+
+	/**
+	 * The list of participants in the session.
+	 */
+	get participants(): HelixSharedChatSessionParticipant[] {
+		return this[rawDataSymbol].participants.map(data => new HelixSharedChatSessionParticipant(data, this._client));
+	}
+
+	/**
+	 * The date for when the session was created.
+	 */
+	get createdDate(): Date {
+		return new Date(this[rawDataSymbol].created_at);
+	}
+
+	/**
+	 * The date for when the session was updated.
+	 */
+	get updatedDate(): Date {
+		return new Date(this[rawDataSymbol].updated_at);
+	}
+}
